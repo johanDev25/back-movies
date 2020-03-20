f7fd48e79fd938d5aeb22710e29fb7e00eceefc4
Johan <johan225@hotmail.es>
2020-03-20 11:30:47 -0500

    name

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::MoviesController < ApplicationController
 
   def index
     if params[:query].present?
-      movies = Movie.where("release = ?", params[:query])
+      movies = Movie.where("release = ? OR title = ?", params[:query])
     else
       movies = Movie.all
     end
